ea2d411959b37b072a11070fac75e354ba3e6567
Justus Perlwitz <mail@justusperlwitz.com>
2017-10-31 12:05:45 +0100

    Iodine: Make description shorter

--- Formula/iodine.rb
+++ Formula/iodine.rb
@@ -1,5 +1,5 @@
 class Iodine < Formula
-  desc "This is a piece of software that lets you tunnel IPv4 data through a DNS server. This can be usable in different situations where internet access is firewalled, but DNS queries are allowed."
+  desc "This is a piece of software that lets you tunnel IPv4 data through a DNS server"
   homepage "http://code.kryo.se/iodine"
   url "https://github.com/yarrick/iodine/archive/05dc792d86ecada826f67b57de5a49bbe38ec874.tar.gz"
   sha256 "4dceb6782926b96d543ca92c506f41de81b84eb55a28a313641b77a8b3dc56a9"
